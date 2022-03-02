1ee68f98fb6fe80891c67f7fadee1d979977463d
annsonic <s86184@yahoo.com.tw>
2022-03-02 14:14:38 +0800

    Fix for weight factor is computed twice.

--- yolov5/utils/loss.py
+++ yolov5/utils/loss.py
@@ -33,12 +33,13 @@ class BCEBlurWithLogitsLoss(nn.Module):
 
 
 class FocalLoss(nn.Module):
+    """ https://github.com/ultralytics/yolov5/issues/6517 """
     # Wraps focal loss around existing loss_fcn(), i.e. criteria = FocalLoss(nn.BCEWithLogitsLoss(), gamma=1.5)
     def __init__(self, loss_fcn, gamma=1.5, alpha=0.25):
         super().__init__()
         self.loss_fcn = loss_fcn  # must be nn.BCEWithLogitsLoss()
         self.gamma = gamma
-        self.alpha = alpha
+        # self.alpha = alpha
         self.reduction = loss_fcn.reduction
         self.loss_fcn.reduction = 'none'  # required to apply FL to each element
 
@@ -50,9 +51,10 @@ class FocalLoss(nn.Module):
         # TF implementation https://github.com/tensorflow/addons/blob/v0.7.1/tensorflow_addons/losses/focal_loss.py
         pred_prob = torch.sigmoid(pred)  # prob from logits
         p_t = true * pred_prob + (1 - true) * (1 - pred_prob)
-        alpha_factor = true * self.alpha + (1 - true) * (1 - self.alpha)
+        # alpha_factor = true * self.alpha + (1 - true) * (1 - self.alpha)
         modulating_factor = (1.0 - p_t) ** self.gamma
-        loss *= alpha_factor * modulating_factor
+        # loss *= alpha_factor * modulating_factor
+        loss *= modulating_factor  # we don't need alpha factor because it's already considered on BCELoss
 
         if self.reduction == 'mean':
             return loss.mean()
